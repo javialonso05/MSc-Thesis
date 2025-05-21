989ddcf89554b29792e3742b140cda9731e0cc89
javialonso05 <63980841+javialonso05@users.noreply.github.com>
2025-05-21 18:49:33 +0200

    Filtering_analysis

--- src/data/data_processor.py
+++ src/data/data_processor.py
@@ -181,11 +181,14 @@ def build_array(data_dict: dict, category: str = 'Intensity', spw: int = 1, retu
             if return_log:
                 mapping.append([source, core, spw])
 
-    return np.array(variable_array) if not return_log else np.array(variable_array), mapping
+    if not return_log:
+        return np.array(variable_array)
+    return np.array(variable_array), mapping
 
 
 def red_shift_correction(frequency: np.ndarray, signal_array: np.ndarray, reference_signal: np.ndarray = None,
-                         plot_reference: bool = False, v_list = np.linspace(-250, 250, 500)):
+                         plot_reference: bool = False, v_list = np.linspace(-250, 250, 500),
+                         width: float = 10):
     """
     Calculate each signal's speed based on their correlation to a reference signal
     :param frequency: 1D array with the frequencies of the signals in signal_array
@@ -193,6 +196,7 @@ def red_shift_correction(frequency: np.ndarray, signal_array: np.ndarray, refere
     :param reference_signal: signal to be matched by the other signals
     :param plot_reference: flag for plotting the reference signal
     :param v_list: list with the velocities to be searched in the broad search phase
+    :param width: width of the peaks in the reference signal
     :return: corrected signals and velocity list
     """
 
@@ -208,7 +212,6 @@ def red_shift_correction(frequency: np.ndarray, signal_array: np.ndarray, refere
 
         reference_signal = np.zeros_like(frequency)
         for peak_location in peak_loc:
-            width = 20
             reference_signal += np.exp(-((np.arange(len(frequency)) - peak_location) ** 2 / (2 * width ** 2)))
 
     if plot_reference:
@@ -227,14 +230,14 @@ def red_shift_correction(frequency: np.ndarray, signal_array: np.ndarray, refere
     corrected_signals = np.zeros_like(signal_array)
     signal_velocities = []
     for i in tqdm(range(len(signal_array)), desc='Shifting signals'):
-        signal = signal_array[i]
+        signal = np.array(signal_array[i])
         signal[signal < 0] = 0  # Remove negative values
 
         # Find broad alignment
         max_sim = -1
         best_vel = 0
         for v in v_list:
-            shifted_signal = shift_signal(frequency, signal, v)[0]
+            shifted_signal = shift_signal(frequency, signal, v)
             similarity = cosine_similarity(shifted_signal, reference_signal)
 
             if similarity > max_sim:
@@ -243,21 +246,112 @@ def red_shift_correction(frequency: np.ndarray, signal_array: np.ndarray, refere
 
         # Fine-tune alignment
         max_sim = -1
-        dv = 1 / 5
+        dv = 0.5 / 10
         v = best_vel - (1 - dv)
+        n = 0
         while np.abs(dv) > 1e-10:
             v += dv
-            sim = cosine_similarity(reference_signal, shift_signal(frequency, signal, v)[0])
+            sim = cosine_similarity(reference_signal, shift_signal(frequency, signal, v))
 
             if sim < max_sim:
                 dv /= -2
 
             max_sim = sim
+            n += 1
 
-        corrected_signals[i] = shift_signal(frequency, signal, v)[0]
+            # Add exit
+            if v >= best_vel + 1 or v <= best_vel - 1:
+                v = best_vel
+                break
+            if n >= 1000:
+                v = best_vel
+                break
+
+        corrected_signals[i] = shift_signal(frequency, signal, v)
         signal_velocities.append(v)
 
-    return corrected_signals, signal_velocities
+    return signal_velocities
+
+
+def filter_data(data: np.ndarray, filter_type: str, residual = None, **kwargs):
+    """
+
+    :param data: 1D/2D array with the data to be filtered
+    :param filter_type: type of filter to perform
+    :param residual: 1D array with the residual of the data
+    :param kwargs: additional arguments passed to the filter function
+    :return: filtered data
+    """
+
+    if filter_type == 'sigma':
+        # Check residual
+        if residual is None:
+            raise ValueError("Residual cannot be None if the filter type is sigma")
+
+        from src.features.transformations import sigma_filter
+
+        filtered_data = sigma_filter(data, residual, **kwargs)
+
+    elif filter_type == 'subtraction':
+        # Check arguments
+        if residual is None:
+            raise ValueError("Residual cannot be None if the filter type is subtraction")
+        if np.shape(data) != np.shape(residual):
+            raise ValueError("Residual and data must have the same shape")
+
+        filtered_data = data - residual
+
+    elif filter_type == 'savgol':
+        from src.features.transformations import savgol_filter
+
+        filtered_data = savgol_filter(data, **kwargs)
+        filtered_data[filtered_data < 0] = 0
+
+    else:
+        raise ValueError(f'{filter_type} is not recognised as an established filter type')
+
+    return filtered_data
+
+
+def majority_voting(velocity_array: np.ndarray, threshold: float = 5):
+    """
+    Determine a signal's velocity based on the most common velocity given a number of methods.
+    :param velocity_array: array with the velocity values for each signal. Each row should be a signal and each column
+    should correspond to the velocity obtained through an independent method. The first row should always be the raw
+    signal velocity
+    :param threshold: maximum absolute velocity difference still considered a match
+    :return: most_likely_velocity
+    """
+
+    # Initialize variables
+    most_likely_velocity = []
+
+    # Iterate over all signals
+    for velocities in tqdm(velocity_array, desc='Calculating majority velocity'):
+        # Initialize empty lists
+        used = np.full(len(velocities), False)
+        groups = []
+
+        # Iterate over every velocity
+        for i, val in enumerate(velocities):
+            if used[i]:
+                continue
+            v_group = [val]  # List with the velocities that coincide between them
+            used[i] = True   # Flag to not repeat the same values
+            for j in range(i + 1, len(velocities)):
+                if not used[j] and abs(val - velocities[j]) <= threshold:
+                    v_group.append(velocities[j])
+                    used[j] = True
+            groups.append(v_group)
+
+        # Find the majority group (must be > 50% of row length)
+        majority_group = max(groups, key=len)
+        if len(majority_group) > len(velocities) / 2:
+            most_likely_velocity.append(np.mean(majority_group))
+        else:
+            most_likely_velocity.append(np.nan)
+
+    return np.array(most_likely_velocity)
 
 
 if __name__ == "__main__":
@@ -267,8 +361,66 @@ if __name__ == "__main__":
     # with open('Data/Raw/interpolated_data_dict.pkl', 'wb') as f:
     #     pickle.dump(interpolated_data, f)
 
-    interpolated_data = pickle.load(open('Data/Raw/interpolated_data_dict.pkl', 'rb'))
-    intensity_array, mapping = build_array(interpolated_data)
-    f1 = interpolated_data['100132']['core1'][1]['Frequency']
+    """
+    Code for the scatter + density graphs:
+        # Create the figure and axes using GridSpec
+        fig = plt.figure(figsize=(10, 6))
+        grid = plt.GridSpec(4, 4, hspace=0.4, wspace=0.4)
+        main_ax = fig.add_subplot(grid[1:, :-1])  # Scatter plot
+        x_density = fig.add_subplot(grid[0, :-1], sharex=main_ax)  # Top density plot
+        y_density = fig.add_subplot(grid[1:, -1], sharey=main_ax)  # Right density plot
+        # Scatter plot of the vectors
+        main_ax.scatter(data_info['SNR'][idx_2], v_diff_2, label='1', alpha=0.6)
+        main_ax.scatter(data_info['SNR'][idx_1], v_diff_1, label='2', alpha=0.6)
+        main_ax.scatter(data_info['SNR'][idx_0], v_diff_0, label='3', alpha=0.6)
+        main_ax.legend(title='# mismatches')
+        main_ax.set_xlabel('SNR [dB]')
+        main_ax.set_ylabel(r'$\Delta$V [km/s]')
+        # KDE plot for x-axis
+        sns.kdeplot(data_info['SNR'][idx_2], ax=x_density, fill=False, color='tab:blue')
+        sns.kdeplot(data_info['SNR'][idx_1], ax=x_density, fill=False, color='tab:orange')
+        sns.kdeplot(data_info['SNR'][idx_0], ax=x_density, fill=False, color='tab:green')
+        # x_density.axis('off')
+        x_density.set_xlabel('')
+        # KDE plot for y-axis (horizontal)
+        sns.kdeplot(y=v_diff_2, ax=y_density, fill=False, color='tab:blue')
+        sns.kdeplot(y=v_diff_1, ax=y_density, fill=False, color='tab:orange')
+        sns.kdeplot(y=v_diff_0, ax=y_density, fill=False, color='tab:green')
+        # y_density.axis('off')
+        plt.show()
+    """
 
-    corrected_signals, red_shift_velocities = red_shift_correction(frequency=f1, signal_array=intensity_array)
+    # Load signals
+    interpolated_data = pickle.load(open('Data/Raw/interpolated_data_dict.pkl', 'rb'))
+
+    # Build arrays
+    f1 = interpolated_data['100132']['core1'][1]['Frequency']
+    intensity_array, mapping = build_array(interpolated_data, category='Intensity')
+    residual_array = build_array(interpolated_data, category='Residual', return_log=False)
+
+    # Load signal data
+    data_info = pd.read_csv('Data/data_info.csv')
+
+    # Calculate velocities
+    filtered_data_10 = []
+    filtered_data_20 = []
+    velocity_array_10 = [red_shift_correction(f1, intensity_array, width=10)]
+    if not np.any(intensity_array < 0):
+        print('Intensity gets edited')
+        intensity_array = build_array(interpolated_data, category='Intensity')[0]
+    velocity_array_20 = [red_shift_correction(f1, intensity_array, width=20)]
+
+    for method in ['sigma', 'subtraction', 'savgol']:
+        if not np.any(intensity_array < 0):
+            intensity_array = build_array(interpolated_data, category='Intensity')[0]
+        filtered_data_10.append(filter_data(intensity_array, method, residual_array))
+        velocity_array_10.append(red_shift_correction(f1, filtered_data_10[-1], width=10))
+
+        if not np.any(intensity_array < 0):
+            intensity_array = build_array(interpolated_data, category='Intensity')[0]
+
+        filtered_data_20.append(filter_data(intensity_array, method, residual_array))
+        velocity_array_20.append(red_shift_correction(f1, filtered_data_10[-1], width=20))
+
+    best_velocity_10 = majority_voting(np.array(velocity_array_10).T)
+    best_velocity_20 = majority_voting(np.array(velocity_array_20).T)
--- src/features/transformations.py
+++ src/features/transformations.py
@@ -1,6 +1,8 @@
 import numpy as np
+from tqdm import tqdm
 
 
+# TODO: move to a more relevant file
 def cosine_similarity(a, b):
     """
     Calculate cosine similarity between two vectors or a vector and a matrix
@@ -11,10 +13,13 @@ def cosine_similarity(a, b):
     if len(np.shape(a)) != 1 and len(np.shape(b)) != 1:
         raise ValueError("At least one input must be a 1D vector")
 
+    if np.linalg.norm(a) == 0 or np.linalg.norm(b) == 0:
+        return 0
+
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
-def shift_signal(frequency, signal, velocity) -> tuple[np.ndarray, np.ndarray]:
+def shift_signal(frequency, signal, velocity):
     """
     Shift a signal by a given velocity
     :param frequency: 1D array with the frequency channels
@@ -26,4 +31,75 @@ def shift_signal(frequency, signal, velocity) -> tuple[np.ndarray, np.ndarray]:
     c = 5e5
     shifted_frequency = frequency * (1 - velocity / c)
     shifted_signal = np.interp(shifted_frequency, frequency, signal, left=0, right=0)
-    return shifted_signal, shifted_frequency
+    return shifted_signal
+
+
+def sigma_filter(intensity_array: np.ndarray, residual_array: np.ndarray, threshold: int = 3):
+    """
+    Filter the signal by removing all data-points smaller than a given threshold based on the residual
+    :param intensity_array:
+    :param residual_array:
+    :param threshold:
+    :return: filtered_data
+    """
+
+    # Check input
+    if np.shape(intensity_array) != np.shape(residual_array):
+        raise ValueError('Shapes of intensity and residual must be equal')
+
+
+    sigma = np.std(intensity_array, axis=1)
+    filtered_data = np.zeros_like(intensity_array)
+    for i in range(len(sigma)):
+        signal = np.array(intensity_array[i])
+        signal[signal < threshold * sigma[i]] = 0
+
+        while np.all(signal == 0):
+            threshold -= 0.1
+            signal = np.array(intensity_array[i])
+            signal[signal < threshold * sigma[i]] = 0
+
+        filtered_data[i] = signal
+
+    return filtered_data
+
+
+def savgol_filter(data_array: np.ndarray, window_length: int = 200, polyorder: int = 3):
+    """
+    Use a Savitzky-Golay filter to smooth the signal
+    :param data_array: data to smooth
+    :param window_length: number of points to consider in the smoothing. Default is 25 based on Ishikawa et al. (2013)
+    :param polyorder: order of the polynomial to use. Default is 3.
+    :return: filtered_data
+    """
+
+    from scipy.signal import savgol_filter
+
+    filtered_signal = savgol_filter(data_array, window_length, polyorder)
+    return filtered_signal
+    
+
+def manually_compare_signals(freq, signals, peaks: list = None, labels: list = None):
+    import matplotlib.pyplot as plt
+
+    if peaks is None:
+        peaks = [220398.684, 219949.433, 219560.358]
+
+    for i in range(len(signals)):
+        fig, ax = plt.subplots(4, 1, sharex=True, figsize=(12, 8))
+
+        ax[0].plot(freq, signals[i][0])
+        ax[1].plot(freq, signals[i][1], color='tab:orange')
+        ax[2].plot(freq, signals[i][2], color='tab:green')
+        ax[3].plot(freq, signals[i][3], color='tab:red')
+
+        for peak in peaks:
+            for j in range(4):
+                ax[j].axvline(peak, color='black', linestyle='--')
+
+        fig.supxlabel('Frequency [MHz]', fontsize=14)
+        fig.supylabel('Intensity [Jy]', x=0.04, fontsize=14)
+        fig.legend(loc='right', title=r'$\Delta$V [km/s]')
+        plt.show()
+
+        input()
