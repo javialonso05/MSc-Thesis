4c58e94fb716fb628860b9f523fef52157b8f8db
javialonso05 <63980841+javialonso05@users.noreply.github.com>
2025-06-10 11:10:49 +0200

    self._update_affinity created

--- src/models/agglomerative.py
+++ src/models/agglomerative.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import silhouette_score
 from tqdm import tqdm
 
 from sklearn.neighbors import NearestNeighbors
@@ -116,45 +117,6 @@ class AgglomerativeClustering:
             for j in range(self.k_neighbors):
                 self.weights[i][indices[i][j]] = np.exp(-distances[i][j]**2 / self.sigma2)
 
-    def _calculate_linkage(self, linkage_type: str):
-        """
-        Calculate indegree or outdegree of all vertices
-        :param linkage_type: (str) either "indegree" or "outdegree"
-        :return:
-        """
-
-        # Check inputs
-        if self.weights is None or len(self.weights) == 0:
-            raise ValueError('self.weights is not defined')
-
-        if linkage_type != "indegree" and linkage_type != "outdegree":
-            raise ValueError('linkage_type must be "indegree" or "outdegree"')
-
-        # Define variables
-        n_samples = self.weights.shape[0]
-        n_clusters = len(np.unique(self.labels_))
-
-        linkage = np.zeros([n_samples, n_clusters])
-        for i in range(n_samples):  # Vertex with respect to which indegree is calculated
-            for j in range(n_clusters):  # Cluster to which vertex i is calculating its affinity
-                if j == self.labels_[i]:
-                    # Vertex i belongs to j
-                    continue
-
-                linkage_list = []
-                cluster_idx = np.where(np.array(self.labels_) == j)[0]
-                for k in cluster_idx:  # Vertex in cluster j
-                    if linkage_type == "indegree":
-                        linkage_list.append(self.weights[k][i])
-                    else:
-                        linkage_list.append(self.weights[i][k])
-
-                linkage[i][j] = np.mean(linkage_list)
-
-        if linkage_type == "indegree":
-            self.indegree = linkage
-        else:
-            self.outdegree = linkage
 
     def _calculate_affinity(self):
         """
@@ -162,25 +124,104 @@ class AgglomerativeClustering:
         :return:
         """
 
-        self._calculate_linkage("indegree")
-        self._calculate_linkage("outdegree")
-        self.point_affinity_matrix = self.indegree * self.outdegree
-
-        n_clusters = len(np.unique(self.labels_))
+        labels = np.array(self.labels_)
+        n_clusters = len(np.unique(labels))
         self.cluster_affinity_matrix = np.zeros((n_clusters, n_clusters))
+
+        cluster_indices = [np.where(labels == i)[0] for i in range(n_clusters)]
         for i in range(n_clusters):
-            cluster_i_idx = np.where(np.array(self.labels_) == i)[0]
+            idx_i = cluster_indices[i]
+            len_i = len(idx_i)
             for j in range(n_clusters):
                 if i == j:
                     continue
 
-                cluster_j_idx = np.where(np.array(self.labels_) == j)[0]
+                idx_j = cluster_indices[j]
+                len_j = len(idx_j)
 
-                # Calculate cluster affinity
-                W_ca = self.weights[cluster_i_idx][:, cluster_j_idx]
-                W_cb = self.weights[cluster_j_idx][:, cluster_i_idx]
-                self.cluster_affinity_matrix[i][j] = (np.sum(np.matmul(np.sum(W_ca, axis=0), W_cb)) / len(W_ca)**2 +
-                                                      np.sum(np.matmul(np.sum(W_cb, axis=0), W_ca)) / len(W_cb)**2)
+                # Sparse submatrices
+                W_ca = self.weights[idx_i][:, idx_j]
+                W_cb = self.weights[idx_j][:, idx_i]
+
+                # Compute row/column sums efficiently
+                sum_W_ca = np.array(W_ca.sum(axis=0)).ravel()
+                sum_W_cb = np.array(W_cb.sum(axis=0)).ravel()
+
+                # Perform dot products
+                term1 = sum_W_ca @ W_cb @ np.ones(len_i) / (len_i ** 2)
+                term2 = sum_W_cb @ W_ca @ np.ones(len_j) / (len_j ** 2)
+
+                self.cluster_affinity_matrix[i, j] = term1 + term2
+
+    def _update_affinity(self, a, b):
+        """
+        Update the affinity matrix - Call only after updating self.labels_
+        :param a: Label of the first merged cluster
+        :param b: Label of the second merged cluster
+        :return:
+        """
+
+        # Generate new affinity matrix
+        new_affinity = np.zeros((len(self.cluster_affinity_matrix - 1), len(self.cluster_affinity_matrix - 1)))
+
+        # Input the affinity of the unmerged clusters
+        new_affinity[:-1, :-1] = np.delete(np.delete(self.cluster_affinity_matrix, [a, b], axis=0), [a, b], axis=1)
+
+        # A_(ab->c) = A_(a->c) + A_(b->c)
+        for i in range(len(self.cluster_affinity_matrix)):
+            if a != i and b != i:
+                new_affinity[-1][i] = self.cluster_affinity_matrix[a][i] + self.cluster_affinity_matrix[b][i]
+
+        # Calculate A_(c->ab)
+        cluster_indices = [np.where(np.array(self.labels_) == i)[0] for i in range(len(new_affinity))]
+        idx_j = cluster_indices[-1]
+        len_j = len(idx_j)
+        for i in range(len(new_affinity)):
+            idx_i = cluster_indices[i]
+            len_i = len(idx_i)
+
+            # Sparse submatrices
+            W_ca = self.weights[idx_i][:, idx_j]
+            W_cb = self.weights[idx_j][:, idx_i]
+
+            # Compute row/column sums efficiently
+            sum_W_ca = np.array(W_ca.sum(axis=0)).ravel()
+            sum_W_cb = np.array(W_cb.sum(axis=0)).ravel()
+
+            # Perform dot products
+            term1 = sum_W_ca @ W_cb @ np.ones(len_i) / (len_i ** 2)
+            term2 = sum_W_cb @ W_ca @ np.ones(len_j) / (len_j ** 2)
+
+            new_affinity[i, -1] = term1 + term2
+
+        self.cluster_affinity_matrix = new_affinity
+
+    def _compute_affinity(self, i, j, cluster_indices):
+        """
+        Calculate the affinity between clusters using indegree and outdegree
+        :return:
+        """
+
+        idx_i = cluster_indices[i]
+        len_i = len(idx_i)
+
+        idx_j = cluster_indices[j]
+        len_j = len(idx_j)
+
+        # Sparse submatrices
+        W_ca = self.weights[idx_i][:, idx_j]
+        W_cb = self.weights[idx_j][:, idx_i]
+
+        # Compute row/column sums efficiently
+        sum_W_ca = np.array(W_ca.sum(axis=0)).ravel()
+        sum_W_cb = np.array(W_cb.sum(axis=0)).ravel()
+
+        # Perform dot products
+        term1 = sum_W_ca @ W_cb @ np.ones(len_i) / (len_i ** 2)
+        term2 = sum_W_cb @ W_ca @ np.ones(len_j) / (len_j ** 2)
+
+        return i, j, term1 + term2
+
 
     def fit(self, data: np.ndarray):
         """
@@ -188,9 +229,13 @@ class AgglomerativeClustering:
         :param data: (array) shape (n_samples, n_features)
         :return:
         """
+        from joblib import Parallel, delayed
+
+        #TODO: improve time performance
 
         # Initial clustering
         self._initialize_clustering(data)
+
         clusters = np.unique(self.labels_).tolist()
 
         n_clusters = len(clusters)
@@ -198,7 +243,7 @@ class AgglomerativeClustering:
 
         # Build k-NN graph
         self._build_weighted_knn_graph(data)
-        for i in tqdm(range(n_clusters - 1), desc='Merging clusters'):
+        for _ in tqdm(range(n_clusters - 1), desc='Merging clusters'):
             # Compute affinity
             self._calculate_affinity()
 
@@ -239,14 +284,55 @@ class AgglomerativeClustering:
                     break
 
 
+def compare_clusterings(list_of_labels: list, plot: bool = True, legend: list = None):
+    """
+    Compare the output of several GDL clusterings
+    :param list_of_labels: list with several label_history lists from several GDL clusterings
+    :param plot: flag for plotting the results
+    :return:
+    """
+
+    n_clusters = []
+    for i in range(len(list_of_labels)):
+        n_clusters.append([len(np.unique(list_of_labels[i][j])) for j in range(len(list_of_labels[i]))])
+
+    max_cluster = np.inf
+    min_cluster = 0
+    for i in range(len(n_clusters)):
+        max_cluster = min(max_cluster, max(n_clusters[i]))
+        min_cluster = max(min_cluster, min(n_clusters[i]))
+
+    coincidence = []
+    for n in range(max_cluster, min_cluster - 1, -1):
+        sub_coincidence = []
+        for i in range(len(n_clusters)):
+            for j in range(i + 1, len(n_clusters)):
+                idx_i = n_clusters[i].index(n)
+                idx_j = n_clusters[j].index(n)
+
+                sub_coincidence.append(adjusted_rand_score(list_of_labels[i][idx_i], list_of_labels[j][idx_j]))
+
+        coincidence.append(sub_coincidence)
+
+    if plot:
+        plt.plot(range(max_cluster, min_cluster - 1, -1), coincidence)
+        plt.legend(legend)
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Adjusted Rand Score')
+        plt.show()
+
+    return coincidence
+
 
 if __name__ == '__main__':
     from src.data.data_processor import build_array, filter_data
     from src.features.transformations import shift_signal
 
+    from sklearn.metrics import adjusted_rand_score
     import matplotlib.pyplot as plt
     import pandas as pd
     import pickle
+
 
     # Load raw data
     interpolated_data = pickle.load(open('Data/Raw/interpolated_data_dict.pkl', 'rb'))
@@ -272,16 +358,48 @@ if __name__ == '__main__':
     shifted_mapping = [mapping[i] for i in range(len(mapping)) if not np.isnan(best_v[i])]
 
     ## Filter data
-    sigma_signal = filter_data(np.asarray(shifted_signals), 'sigma', shifted_residual)
-    subtraction_signal = filter_data(np.asarray(shifted_signals), 'subtraction', shifted_residual)
-    savgol_signal = filter_data(np.asarray(shifted_signals), 'savgol')
+    # subtraction_signal = filter_data(np.asarray(shifted_signals), 'subtraction', shifted_residual)
+    # sigma_signal = filter_data(np.asarray(shifted_signals), 'sigma', shifted_residual)
+    # savgol_signal = filter_data(np.asarray(shifted_signals), 'savgol')
 
     # Find old data index
     shifted_mapping = [shifted_mapping[i][:-1] for i in range(len(shifted_mapping))]
 
     # Train AC algorithm
-    ac = AgglomerativeClustering(k_neighbors=100, k0_neighbors=1, distance_metric='cosine')
-    ac.fit(subtraction_signal)
+    # TODO: batch training
+
+    # for filter in [None, 'sigma', 'savgol']:
+    #     if filter is not None:
+    #         signals = filter_data(np.asarray(shifted_signals), filter, shifted_residual)
+    #     else:
+    #         signals = shifted_signals
+    #
+    #     ac = AgglomerativeClustering(k_neighbors=25, distance_metric='cosine')
+    #     ac.fit(signals)
+    #     with open(f'models/agglomerative/full_{filter}_25_neighbors.pkl', 'wb') as f:
+    #         pickle.dump(ac, f)
+    #
+    # subtraction_signal = filter_data(np.asarray(shifted_signals), 'subtraction', shifted_residual)
+    # for spw in [0, 1]:
+    #     ac = AgglomerativeClustering(k_neighbors=25, distance_metric='cosine')
+    #     if spw == 0:
+    #         ac.fit(subtraction_signal[:, :10000])
+    #     else:
+    #         ac.fit(subtraction_signal[:, 10000:])
+    #
+    #     with open(f'models/agglomerative/full_subtraction_25_neighbors_spw{spw}.pkl', 'wb') as f:
+    #         pickle.dump(ac, f)
+
+
+
+    # TODO: train on reduced dataset
+    # for p in [10, 25, 50]:
+    #     signals = subtraction_signal[np.random.randint(len(subtraction_signal), size=len(subtraction_signal)//p)]
+    #     ac = AgglomerativeClustering(k_neighbors=25, k0_neighbors=1, distance_metric='cosine')
+    #     ac.fit(signals)
+    #
+    #     with open(f'models/agglomerative/{p}pc_subtraction_model_25_neighbors.pkl', 'wb') as f:
+    #         pickle.dump(ac, f)
 
     """
     for i in range(len(ac.label_history)):
@@ -298,3 +416,13 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
     """
+
+    subtraction_model = pickle.load(open('models/agglomerative/full_ac_model_25_neighbors.pkl', 'rb'))
+    raw_model = pickle.load(open('models/agglomerative/full_None_25_neighbors.pkl', 'rb'))
+    sigma_model = pickle.load(open('models/agglomerative/full_sigma_25_neighbors.pkl', 'rb'))
+    savgol_model = pickle.load(open('models/agglomerative/full_savgol_25_neighbors.pkl', 'rb'))
+
+    label_list = [subtraction_model.label_history,
+                  sigma_model.label_history, savgol_model.label_history]
+    coincidence = compare_clusterings(label_list, legend=['SU-SI', 'SU-SG', 'SI-SG'])
+
